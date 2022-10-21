9112d6f2ea7e983be707d31bc2dd4fcc319ea8b0
Samuel <s.ogundare@gmail.com>
2022-10-21 11:48:55 -0400

    removed unnecessary lines in models

--- auctions/models.py
+++ auctions/models.py
@@ -4,35 +4,14 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
 import requests
-
-
-
-# TODO
-# ADD USERS TO RELEVANT MODELS
-
-
 class User(AbstractUser):
     pass
 
-
-# class Bid(models.Model):
-#     bids = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-#     # starting_bid = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="starting_bid")
-#     time_submitted = models.DateTimeField(blank=False, default=timezone.now)
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     def __str__(self):
-#         return f"{self.bids}"
-    
-
-
 class Listing(models.Model):
-    # listing_image = models.ImageField(upload_to='uploads')
-    # id = models.AutoField
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     details = models.TextField(max_length=300)
-    # price = models.ForeignKey(Bid, on_delete=models.CASCADE, blank=True, related_name="bid_price")
     CATEGORY_CHOICES = [
         ("FOOD", "FOOD"),
         ("ELECTRONICS", "ELECTRONICS"),
@@ -55,15 +34,10 @@ class Listing(models.Model):
         default="NONE",
     )
     created = models.DateTimeField
-
     image_url = models.ImageField(null=True, blank=True, upload_to='images/')
-    # image_url_2 = models.ImageField(blank=True)
     active = models.BooleanField(default=True)
     time_submitted = models.DateTimeField(blank=False, default=timezone.now)
     winner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="winner")
-
-    # def __init__(self):
-    #     return f"{self.title}"
     def __str__(self):
         return f"{self.title}"
 
