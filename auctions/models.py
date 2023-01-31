bef3ed930953c697ad0782036216f0b7692ee09a
Samuel Ogundare <s.ogundare@gmail.com>
2023-01-31 17:29:05 -0500

    commit prepare for hero

--- auctions/models.py
+++ auctions/models.py
@@ -1,5 +1,5 @@
 from tkinter import CASCADE, Image
-from typing_extensions import Required
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
