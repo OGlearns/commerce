3b6cb74682d3801ac4d7c1ec2f0971abcf4afa0c
Samuel Ogundare <s.ogundare@gmail.com>
2023-01-31 21:48:56 -0500

    prepare for deploy

--- auctions/models.py
+++ auctions/models.py
@@ -1,5 +1,6 @@
 from tkinter import CASCADE, Image
 
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
--- commerce/settings.py
+++ commerce/settings.py
@@ -25,11 +25,11 @@ DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
+PILLOW_VERSION = 8.0
 # Application definition
 
 INSTALLED_APPS = [
