7d092a1d807501f3a218b738cbc376972e77da7a
Samuel Ogundare <s.ogundare@gmail.com>
2023-02-01 02:14:26 -0500

    updated hosts

--- commerce/settings.py
+++ commerce/settings.py
@@ -29,7 +29,7 @@ DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 PILLOW_VERSION = '9.4.0'
 # Application definition
