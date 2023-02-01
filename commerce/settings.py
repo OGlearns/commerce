26d45204f1ac25e54b0031c30c1c6ad033d529c0
Samuel Ogundare <s.ogundare@gmail.com>
2023-01-31 23:04:57 -0500

    Remove hardcoded Django secret key

--- commerce/settings.py
+++ commerce/settings.py
@@ -24,7 +24,7 @@ DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
+# SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
